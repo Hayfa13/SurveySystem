5627a20142acae6be8da56d48639e122ad1d8fec
GitHub*hayfa13* <hayfafisel@gmail.com>
2023-11-24 08:58:21 +0530

    SurverySystem

--- main.js
+++ main.js
@@ -3,6 +3,5 @@ document.getElementById('surveyForm').addEventListener('submit', function (event
 
     document.getElementById('surveyForm').style.display = 'none';
     document.getElementById('head').style.display = 'none';
-    document.getElementById('subheading').style.display = 'none';
     document.getElementById('thankYouMessage').style.display = 'block';
 });
